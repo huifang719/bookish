5bad6cd7bb23c497cbaa23a80a52df764421ed56
huifang <hui-f.gu@hotmail.com>
2023-03-06 05:18:51 -0800

    add user model

--- Models/Book.cs
+++ Models/Book.cs
@@ -2,6 +2,7 @@
 {
     public class Book
     {
+        public int Id { get; set; } 
         public string OLID { get; set; } = null!;
         public string Name { get; set; } = null!;
         public double Price { get; set; }
--- WeatherForecast.cs
+++ WeatherForecast.cs
@@ -1,8 +1,8 @@
-namespace Bookish
+namespace test
 {
     public class WeatherForecast
     {
-        public DateOnly Date { get; set; }
+        public DateTime Date { get; set; }
 
         public int TemperatureC { get; set; }
 
